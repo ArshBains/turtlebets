001a05a67666a86676fe688b5533fb581f2cca22
ArshBains <56476189+ArshBains@users.noreply.github.com>
2019-10-13 11:46:19 +0530

    Add files via upload

---
+++ k.py
@@ -0,0 +1,22 @@
+import turtle
+
+k = turtle.Turtle()
+
+k.pencolor("blue")
+k.hideturtle()
+
+k.left(90)
+k.forward(200)
+k.penup()
+k.back(100)
+k.right(45)
+k.pendown()
+k.forward(141)
+k.penup()
+k.back(141)
+k.right(90)
+k.pendown()
+k.forward(141)
+
+turtle.done()
+# ArshBains
